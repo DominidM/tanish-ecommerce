dc539e6aceb6c6985a513d18237d240d37c96719
Juan Dominid Muñoz Eslava <dominidzero@gmail.com>
2026-08-28 07:43:03 -0500

    feat: initialize TANISH Inventory plugin

---
+++ plugins/tanish-inventory/tanish-inventory.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: TANISH Inventory
+ * Plugin URI: https://github.com/DominidM/tanish-ecommerce
+ * Description: Gestión interna de inventario integrada con WooCommerce para TANISH S.A.C.
+ * Version: 0.1.0
+ * Author: Juan Dominid Muñoz Eslava
+ * Text Domain: tanish-inventory
+ */
+
+defined('ABSPATH') || exit;
